e5cfaf0caa87133bb54ffa2cf321dbb131f4294f
iamkanguk97 <iamkanguk97@kakao.com>
2022-12-27 02:58:05 +0900

    [이취코-정렬-PART2] 직관적인 Quick sort 구현 완료

--- 이취코/정렬/PART2/6-4.py
+++ 이취코/정렬/PART2/6-4.py
@@ -4,21 +4,31 @@
 # 3) 왼쪽과 오른쪽을 Search하다가 겹치게 되는 경우 PIVOT과 작은수를 SWAP
 
 def quick_sort(array, start, end):
-    pivot = start   # 첫 원소는 PIVOT으로 설정
-    list_left = pivot + 1   # PIVOT을 제외한 리스트의 맨 왼쪽 IDX
-    list_right = end   # PIVOT을 제외한 리스트의 맨 오른쪽 IDX
+    if start >= end:   # 리스트의 원소가 1개인 경우 함수 종료
+        return
+
+    pivot = start
+    lt = start + 1   # 리스트 왼쪽 원소 (PIVOT보다 1칸 오른쪽)
+    rt = end   # 리스트 오른쪽 원소 (맨 끝)
+
+    # 원소 탐색 시작
+    while lt <= rt:
+        while lt <= end and array[pivot] >= array[lt]:   # PIVOT보다 큰 데이터 탐색
+            lt += 1
+        while rt > start and array[pivot] <= array[rt]:   # PIVOT보다 작은 데이터 탐색
+            rt -= 1
+
+        if lt > rt:   # left와 right가 교차된 순간에는 right와 pivot을 swap
+            array[rt], array[pivot] = array[pivot], array[rt]
+        else:
+            array[lt], array[rt] = array[rt], array[lt]
+
+
+    # 분할 이후 왼쪽 리스트와 오른쪽 리스트 quick_sort 진행
+    quick_sort(array, start, rt - 1)
+    quick_sort(array, rt + 1, end)
+
     
-    while True:
-        # 왼쪽에서 Search 시작
-        while array[list_left] > array[pivot]:   # PIVOT보다 큰 수를 찾을 때 까지
-            list_left += 1
-        # 오른쪽에서 Search 시작
-        while array[list_right] < array[pivot]:   # PIVOT보다 작은 수를 찾을 떄 까지
-            list_right -= 1
-    
-
-
-
-# array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
-# quick_sort(array, 0, len(array)-1)
-# print(array)
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+quick_sort(array, 0, len(array)-1)
+print(array)
