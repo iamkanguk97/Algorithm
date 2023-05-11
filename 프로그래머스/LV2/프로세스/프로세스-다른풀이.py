94b05b5c0aebbc1b369d789d4138932bfe204b81
KANGUK LEE <iamkanguk97@kakao.com>
2023-05-11 17:56:19 +0900

    [프로그래머스-LV2] 프로세스 다른풀이

---
+++ 프로그래머스/LV2/프로세스/프로세스-다른풀이.py
@@ -0,0 +1,17 @@
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+            
+
+
+priorities = [2, 1, 3, 2]
+print(solution(priorities, 2))
