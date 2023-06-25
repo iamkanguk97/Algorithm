4c964a34d56f9810348e07cd47912007214d1d3a
KANGUK LEE <iamkanguk97@kakao.com>
2023-06-25 21:50:06 +0900

    [이취코-DFS/BFS-PART2] 음료수 얼려먹기 재풀이

--- 이취코/DFS와 BFS/PART2/실전문제/음료수 얼려먹기.py
+++ 이취코/DFS와 BFS/PART2/실전문제/음료수 얼려먹기.py
@@ -1,31 +1,30 @@
-# DFS와 BFS PART2 실전문제) 음료수 얼려먹기
+"""
+이취코 DFS/BFS PART2: 음료수 얼려먹기
+난이도: 1.5 / 3
+"""
 
-N, M = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(N)]
+n, m = map(int, input().split())
+ice = [list(map(int, input())) for _ in range(n)]
 
-# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들 방문
 def dfs(x, y):
-    # 좌표가 범위 벗어나면 종료
-    if x <= -1 or x >= N or y <= -1 or y >= M:
+    if x < 0 or y < 0 or x >= n or y >= m:   # 좌표 넘어가면 함수 종료
         return False
-    
-    # 현재 노드를 방문하지 않음
-    if graph[x][y] == 0:
-        graph[x][y] = 1 # 방문처리
 
-        # 상하좌우 모두 재귀적으로 DFS 함수 호출
-        dfs(x+1, y) 
+    if ice[x][y] == 0:
+        ice[x][y] = 1
         dfs(x-1, y)
-        dfs(x, y+1)
+        dfs(x+1, y)
         dfs(x, y-1)
+        dfs(x, y+1)
         return True
     return False
 
 
-result = 0
-for i in range(N):
-    for j in range(M):
-        if dfs(i, j) == True:
-            result += 1
 
-print(result)
+count = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            count += 1
+
+print(count)
