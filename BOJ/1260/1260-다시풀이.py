9e18e4d38e2befe70dbac0d2ab37de8ccf131647
KANGUK LEE <iamkanguk97@kakao.com>
2023-08-29 14:36:30 +0900

    [BOJ-1260] DFS와 BFS 다시풀이

--- BOJ/1260/1260-다시풀이.py
+++ BOJ/1260/1260-다시풀이.py
@@ -1,18 +1,37 @@
 # BOJ 1260번: DFS와 BFS (S2)
 
-def dfs(node):
-    print(graph)
-    print(node)
-    return
+def dfs(idx):
+    global visited
+    visited[idx] = True
+    print(idx, end=' ')
+    for next in range(1, n+1):
+        if not visited[next] and graph[idx][next]:
+            dfs(next)
 
-def bfs(graph, node):
-    return
-
+def bfs():
+    global q, visited
+    while q:
+        curr = q.pop(0)
+        print(curr, end=' ')
+        for next in range(1, n+1):
+            if not visited[next] and graph[curr][next]:
+                visited[next] = True
+                q.append(next)
 
 n, m, v = map(int, input().split())
-graph = [tuple(map(int, input().split())) for _ in range(m)]
+graph = [[False] * (n+1) for _ in range(n+1)]
+visited = [False] * (n+1)
 
-visited_dfs = []
-visited_bfs = []
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
 
 dfs(v)
+print()
+
+visited = [False] * (n+1)
+q = [v]
+visited[v] = True
+
+bfs()
